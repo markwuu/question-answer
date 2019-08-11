638c1004cb1c1bfd3fe39285bc636a68e480e55a
Mark W <markwuu@gmail.com>
2019-08-11 15:32:13 -0700

    Added validation to question and answer schemas

--- models/answer.js
+++ models/answer.js
@@ -3,7 +3,12 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var AnswerSchema = new Schema({
-  answer: { type: String, required: true, min: 5, max: 100 }
+  answer: {
+    type: String,
+    required: true,
+    min: [5, "The answer must be at least 5 characters"],
+    max: 100
+  }
 });
 
 module.exports = mongoose.model("Answer", AnswerSchema);
--- models/question.js
+++ models/question.js
@@ -3,7 +3,12 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var QuestionSchema = new Schema({
-  question: { type: String, required: true, min: 5, max: 100 }
+  question: {
+    type: String,
+    required: true,
+    min: [5, "The question must be at least 5 characters"],
+    max: 100
+  }
 });
 
 module.exports = mongoose.model("Question", QuestionSchema);
